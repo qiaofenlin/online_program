f41510573497a7896cae9d006fa0a5e711ceb517
乔风鳞 <790872612@qq.com>
2018-12-09 10:50:17 +0800

    开始项目。

---
+++ src/main/java/com/example/online_program/OnlineProgramApplication.java
@@ -0,0 +1,12 @@
+package com.example.online_program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineProgramApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/online_program/OnlineProgramApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.online_program;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineProgramApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
